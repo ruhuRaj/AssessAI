e3301cdb5e25f29c52f09c3d937bc9b9f86f7f37
Randhir Kumar Raj <randhirrajak9431@gmail.com>
2026-05-28 18:28:52 +0530

    some issue fixed on otps sending

--- backend/src/services/emailService.ts
+++ backend/src/services/emailService.ts
@@ -1,7 +1,9 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
