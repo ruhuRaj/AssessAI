2a1347c0e5a84c7d85e301ab345409ca8a65c111
Randhir Kumar Raj <randhirrajak9431@gmail.com>
2026-05-29 11:24:54 +0530

    using brevo for the otps sending

--- backend/src/services/emailService.ts
+++ backend/src/services/emailService.ts
@@ -3,6 +3,9 @@ import dns from "dns";
 
 dns.setDefaultResultOrder("ipv4first");
 
+console.log("SMTP_HOST =", process.env.SMTP_HOST);
+console.log("SMTP_USER =", process.env.SMTP_USER);
+
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || "smtp-relay.brevo.com",
   port: Number(process.env.SMTP_PORT) || 465,
